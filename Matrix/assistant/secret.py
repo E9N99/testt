c5eeac20684e1a93a49160dd764a41a248d4e96e
BiLaL <97440505+E9N99@users.noreply.github.com>
2024-05-24 20:58:18 +0300

    Update secret.py

--- Matrix/assistant/secret.py
+++ Matrix/assistant/secret.py
@@ -5,7 +5,7 @@ import re
 from telethon.events import CallbackQuery
 from telethon.tl.functions.users import GetUsersRequest
 
-from zelz import zedub
+from Matrix import zedub
 from ..sql_helper.globals import gvarstatus
 
 
@@ -18,8 +18,8 @@ async def on_plug_in_callback_query_handler(event):
         zzz = await zedub.get_entity(ussr)
     except ValueError:
         zzz = await zedub(GetUsersRequest(ussr))
-    if os.path.exists("./zelz/secret.txt"):
-        jsondata = json.load(open("./zelz/secret.txt"))
+    if os.path.exists("./Matrix/secret.txt"):
+        jsondata = json.load(open("./Matrix/secret.txt"))
         try:
             message = jsondata[f"{timestamp}"]
             userid = message["userid"]
